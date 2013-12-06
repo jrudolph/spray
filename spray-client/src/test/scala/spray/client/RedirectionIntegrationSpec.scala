c7240634adc1699a78e734a2a45cb869e863e7fd
Johannes Rudolph <johannes.rudolph@gmail.com>
2013-12-06 09:24:28 +0100

    = client: cleanup and fix license header

--- spray-client/src/test/scala/spray/client/RedirectionIntegrationSpec.scala
+++ spray-client/src/test/scala/spray/client/RedirectionIntegrationSpec.scala
@@ -1,9 +1,25 @@
+/*
+ * Copyright © 2011-2013 the spray project <http://spray.io>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package spray.client
 
 import org.specs2.mutable.Specification
 import org.specs2.time.NoTimeConversions
 import com.typesafe.config.ConfigFactory
-import akka.actor.{ ActorRef, Actor, Props, ActorSystem }
+import akka.actor.{ Actor, Props, ActorSystem }
 import spray.can.Http
 import spray.http._
 import spray.http.HttpHeaders.Location
@@ -15,10 +31,6 @@ import scala.concurrent.duration._
 import akka.util.Timeout
 import java.net.InetSocketAddress
 
-/**
- * Date: 05/10/2013
- * Time: 22:17
- */
 class RedirectionIntegrationSpec extends Specification with NoTimeConversions {
   sequential
 
